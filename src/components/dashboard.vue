7034668d1fde3fc0c5fb7bf102cfa22dc94662ac
luolinjia <360512239@qq.com>
2018-03-08 17:27:52 +0800

    v1.0.2 done

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -580,8 +580,8 @@
 <style lang="less">
     .report-tree {
         position:absolute;
-        width:880px;
-        height:800px;
+        width:60%;
+        height:50%;
         top:50%;
         left:50%;
         transform:translate(-50%,-50%);
